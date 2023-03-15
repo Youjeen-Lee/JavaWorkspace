fc00e5def9e3401485e499b46c87fb82bf6ebf16
Youjin-Lee47 <idfw777@gmail.com>
2023-03-15 11:40:20 +0900

    Ex03

---
+++ src/polyclass/Ex03_02.java
@@ -0,0 +1,13 @@
+package polyclass;
+
+public class Ex03_02 {
+    public static void main(String[] args) {
+
+        System.out.printf("%d", 100, 200);
+        System.out.printf("\n");
+        System.out.printf("%d %d", 100, 200);
+        System.out.printf("\n");
+
+
+    }
+}
