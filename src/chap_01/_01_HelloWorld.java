956d2755b0ad4100a58f9431a2484cd2e02bf265
Youjin <idfw777@gmail.com>
2023-03-15 09:35:00 +0900

    Go Github!

--- src/chap_01/_01_HelloWorld.java
+++ src/chap_01/_01_HelloWorld.java
@@ -3,5 +3,8 @@ package chap_01;
 public class _01_HelloWorld {
     public static void main(String[] args) {
         System.out.println("호두과자 맛있다");
+        System.out.println("Hello Github!!");
+
+
     }
 }
