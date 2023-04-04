0b1d3bbccd1cb7e398dfadab2c074af9bce4302c
Youjin-Lee47 <idfw777@gmail.com>
2023-04-04 17:16:08 +0900

    Ex03

--- src/polyquiz/PolyQuiz0330.java
+++ src/polyquiz/PolyQuiz0330.java
@@ -3,7 +3,12 @@ package polyquiz;
 public class PolyQuiz0330 {
     public static void main(String[] args) {
         for (int i = 18; i < 90; i++) {
+            // (1) int i는 18부터 90보다 작으면 아래의 수식을 수행한다.
             System.out.printf("%d * %d = %d\n", i / 9, i % 9 + 1, (i / 9) * (i % 9 + 1));
+            // (2)
+
+
+
         }
     }
 }
--- src/polyquiz/_02_StarDiamond.java
+++
@@ -1,9 +0,0 @@
-package polyquiz;
-
-public class _02_StarDiamond {
-    public static void main(String[] args) {
-
-    // 마름모 복수한다...반드시
-
-    }
-}
