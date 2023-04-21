91f8b25c01a95c15f1e8a3e8db1b10b65915f770
Youjin-Lee47 <idfw777@gmail.com>
2023-04-21 14:05:02 +0900

    230420) 자바 퀴즈 복습

--- src/polyquiz/PQ_3n5Multiple.java
+++ src/polyquiz/PQ_3n5Multiple.java
@@ -1,7 +1,7 @@
 package polyquiz;
 import java.util.Scanner;
 
-public class PQ_3n5Multiple {
+public class PQ_3n5Multiple { // 자바 퀴즈 복습
     public static void main(String[] args) { // 3과 5의 배수 판별(연산자)
 
         Scanner s = new Scanner(System.in);
@@ -10,13 +10,13 @@ public class PQ_3n5Multiple {
         System.out.printf("값을 입력하세요 ->");
         a = s.nextInt();
 
-        if(a % 3 == 0) {
-            System.out.printf("3의 배수입니다.\n");
-        } else if(a % 5 ==0) {
-            System.out.printf("5의 배수입니다.\n");
-        } else if(a % 3 == 0 && a % 5 == 0); {
-            System.out.printf("3의 배수와 5의 배수를 모두 만족합니다.");
+        if((a % 3 == 0) && (a % 5 == 0)) {
+            System.out.printf("3의 배수와 5의 배수를 모두 만족합니다.\n");
+        } else if(a % 3 == 0) {
+            System.out.println("3의 배수입니다.");
+        } else if( a % 5 == 0) {
+            System.out.print("5의 배수입니다.");
         }
-
     }
 }
+// 조건문 ()뒤에 ';' 쓰지않게 주의!! ==> 문장이 끝나버려서 조건문이 실행이 안돼 출력문이 무조건 출력됨.
