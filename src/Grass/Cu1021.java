f15af43096886d81e266e34b2778a8f2356b82e3
Youjin-Lee47 <idfw777@gmail.com>
2023-04-26 12:58:54 +0900

    23.04.26 코드업 기초예제 풀기 1021

---
+++ src/Grass/Cu1021.java
@@ -0,0 +1,13 @@
+package Grass;
+
+import java.util.Scanner;
+public class Cu1021 { // 코드업 기초예제 1021 : 단어 1개 입력받아 그대로 출력
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.next();
+
+        System.out.println(s);
+
+    }
+}
