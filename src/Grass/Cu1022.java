f41d6ae277ff3f7f90fb5779d53c2c94561bbe31
Youjin-Lee47 <idfw777@gmail.com>
2023-04-27 11:04:08 +0900

    23.04.27 코드업 기초예제 풀기 1022

---
+++ src/Grass/Cu1022.java
@@ -0,0 +1,13 @@
+package Grass;
+
+import java.util.Scanner;
+
+public class Cu1022 { // 문장 1개 입력받아 그대로 출력
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        System.out.println(s);
+    }
+}
+// next() : 문자 또는 문자열을 공백을 기준으로 한단어 또는 한문자씩 입력받는다.
+// nextLine() : 문자 또는 문장 한 라인 전체를 입력받는다. (대문자로 꼭 구별)
