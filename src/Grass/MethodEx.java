c8959f95476fa9c97e7ccd6b3bbee8a687b434b8
Youjin-Lee47 <idfw777@gmail.com>
2023-05-10 12:25:40 +0900

    (230510) 메소드 함수 기초 예제

---
+++ src/Grass/MethodEx.java
@@ -0,0 +1,18 @@
+package Grass;
+
+public class MethodEx {
+    public static int plus(int v1, int v2, int v3) {
+
+        int result;
+        result = v1 + v2 + v3;
+        return result;
+    }
+    public static void main(String[] args) {
+
+        int hap;
+        hap = plus(100, 200, 300);
+        System.out.printf("100과 200과 300의 plus() 메소드 결과는 : %d\n", hap);
+
+    }
+}
+
