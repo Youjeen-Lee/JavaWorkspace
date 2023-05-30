27a10d8f707bf957cd23fe1c285db30eb249385a
Youjin-Lee47 <idfw777@gmail.com>
2023-05-30 16:34:10 +0900

    (230530) 코드업 기초예제 1038번 Scanner 메소드의 원리 정확하게 알게됨.

--- src/Grass/Cu1038.java
+++ src/Grass/Cu1038.java
@@ -2,7 +2,7 @@ package Grass;
 
 import java.util.Scanner;
 
-public class Cu1038 { // 코드업 기초예제 1038 : 정수 2개 입력받아 합 출력하기
+public class Cu1038 { // 코드업 기초예제 1038 : 정수 2개 입력받아 합 출력하기!
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in); // 2개의 정수가 공백으로 구분되어 입력
         long a = scn.nextLong();
