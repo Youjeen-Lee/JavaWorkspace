e8bf1fb7831a1600311cd4a7f83dfade9e1cd056
Youjin-Lee47 <idfw777@gmail.com>
2023-05-31 11:49:04 +0900

    (230531) 코드업 기초예제 1040번

---
+++ src/Grass/Cu1040.java
@@ -0,0 +1,12 @@
+package Grass;
+
+import java.util.Scanner;
+
+public class Cu1040 { // 코드업 기초예제 1040 : 정수 입력받아 부호 바꿔 출력
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int num = scn.nextInt();
+        System.out.println(num *-1);
+
+    }
+}
