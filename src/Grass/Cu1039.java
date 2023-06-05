bdfc8ff5b37356de9c7c9abc7b925c52bccce84e
Youjin-Lee47 <idfw777@gmail.com>
2023-06-05 23:47:45 +0900

    (230605) 코드업 기초예제 1039번

---
+++ src/Grass/Cu1039.java
@@ -0,0 +1,12 @@
+package Grass;
+
+import java.util.Scanner;
+
+public class Cu1039 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        long a = scn.nextLong();
+        long b = scn.nextLong();
+        System.out.println(a+b);
+    }
+}
