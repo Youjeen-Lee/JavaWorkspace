0e846b718100fd50190b73f4b26127539b292cd0
Youjin-Lee47 <idfw777@gmail.com>
2023-06-23 16:20:28 +0900

    (230623) 코드업 기초예제 1064 삼항연산자로 정수 3개 비교

---
+++ src/Grass/Cu1064.java
@@ -0,0 +1,17 @@
+package Grass;
+
+import java.util.Scanner;
+
+public class Cu1064 { // 1064 : 정수 3개 입력받아 가장 작은 수 출력하기
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        int a = scn.nextInt();
+        int b = scn.nextInt();
+        int c = scn.nextInt();
+
+        System.out.println(((a<b)?a:b)<c?((a<b)?a:b):c);
+
+    }
+}
+// (a > b)? a : b
