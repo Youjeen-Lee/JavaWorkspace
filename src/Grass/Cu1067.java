6aaf81417b19e30b8ad63adb1b2756c9caa46690
Youjin-Lee47 <idfw777@gmail.com>
2023-06-27 12:34:50 +0900

    (230627)코드업 기초예제 1067 : 정수 1개 입력받아 음/양/홀/짝 구분하기!

--- src/Grass/Cu1067.java
+++ src/Grass/Cu1067.java
@@ -2,7 +2,7 @@ package Grass;
 
 import java.util.Scanner;
 
-public class Cu1067 { // 코드업 기초예제 1067 : 정수 1개 입력받아 음/양/홀/짝 구분하기.
+public class Cu1067 { // 코드업 기초예제 1067 : 정수 1개 입력받아 음/양/홀/짝 구분하기!
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
 
